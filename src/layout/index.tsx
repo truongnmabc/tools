efef9d63fc78c87f2c2d4e9c52e81c54f96ec28a
truongnmabc <truongngo@abc-elearning.org>
2025-03-31 17:34:06 +0700

    up

---
+++ src/layout/index.tsx
@@ -0,0 +1,73 @@
+"use client";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+import React from "react";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items2: MenuProps["items"] = [
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+].map((icon, index) => {
+  const key = String(index + 1);
+
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: Array.from({ length: 4 }).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout className="w-screen h-screen">
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" />
+      </Header>
+      <div style={{ padding: "24px 48px" }} className="flex flex-1">
+        <Layout
+          style={{
+            padding: "24px 0",
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Sider style={{ background: colorBgContainer }} width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%" }}
+              items={items2}
+            />
+          </Sider>
+          <Content style={{ padding: "0 24px", minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </div>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
